a2d25f52a44ab4aa8cfbbed9c0398e78410dd272
elehsiang <elehsiang>
2014-03-10 22:06:27 +0800

    add memcpy implemented by C

--- kernel.c
+++ kernel.c
@@ -9,7 +9,12 @@
 
 #include <stddef.h>
 
-void *memcpy(void *dest, const void *src, size_t n);
+//void *memcpy(void *dest, const void *src, size_t n);
+void *memcpy(void *dest, const void *src, size_t n){
+    while(n--){
+        *(char *)dest++ = *(char *)src++;
+    }
+}
 
 int strcmp(const char *a, const char *b) __attribute__ ((naked));
 int strcmp(const char *a, const char *b)
